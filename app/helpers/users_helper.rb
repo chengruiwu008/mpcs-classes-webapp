1291b82d1884ad295072a251b217f4d18b5ead3b
smlance <stefan@lances.net>
2015-01-02 13:37:14 -0600

    Add some code to fix users show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,9 @@ module UsersHelper
     user.first_name + " " + user.last_name
   end
 
+  def user_role_warning
+    "If you update this user's roles (and you are not this user), an e-mail " +
+    "will be sent to them."
+  end
+
 end

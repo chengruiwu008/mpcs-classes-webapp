005a3cbff19011f4e6cf7b1fc702c23436df7883
smlance <stefan@lances.net>
2015-02-08 16:10:56 -0600

    Begin adding code to change bid dropdown selects

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,4 +7,12 @@ $(document).ready(function(event) {
     $("#ranked, #unranked").sortable({
 	connectWith: ".connected-sortable"
     }).disableSelection();
+
+    $("#ranked, #unranked").sortable({
+	stop: function(e, ui) {
+	    console.log(ui);
+	    $(this).val("5");
+	    console.log(ui.item.index());
+	}
+    });
 });

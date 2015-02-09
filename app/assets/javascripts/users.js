c18d449addae37ac49847e74d9c395b947bc581e
smlance <stefan@lances.net>
2015-02-09 14:39:27 -0600

    Let students change course ranks with drag & drop
    - Let students change their course ranks by dragging and dropping them
      in the list
    
    Work on dynamic jQuery bid ranking
    
    Use a simple jQuery loop to update bid ranks

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,9 +10,17 @@ $(document).ready(function(event) {
 
     $("#ranked, #unranked").sortable({
 	stop: function(e, ui) {
-	    console.log(ui);
-	    $(this).val("5");
-	    console.log(ui.item.index());
+	    var dragged = $(ui.item.get());
+
+	    dragged.parent().find("li").each(function(index, elt) {
+		$(this).find("select").val(index + 1);
+	    });
+
+	    // If dropped into the bottom list, set its value to "No pref"
+
+	    // In the controller, update _all_ items (in both lists). We'll
+	    // need to ensure that the right ones end up in the right @lists,
+	    // and inspect the params a bit...
 	}
     });
 });

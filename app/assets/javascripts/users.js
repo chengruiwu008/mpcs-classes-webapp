6aee8bf5cda1d03b97321b85cd0e05e3ed17c9df
smlance <stefan@lances.net>
2015-02-10 14:05:00 -0600

    Let students drag course ranks to "no prefs" list
    - Let students drag course ranks to "no preferences" list

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,15 +12,22 @@ $(document).ready(function(event) {
 	stop: function(e, ui) {
 	    var dragged = $(ui.item.get());
 
-	    dragged.parent().find("li").each(function(index, elt) {
+	    // Change rank by dragging and dropping
+	    if (dragged.parent().attr("id") == "unranked") {
+		dragged.find("select").val("No preference");
+	    }
+
+	    $("#ranked").find("li").each(function(index, elt) {
 		$(this).find("select").val(index + 1);
 	    });
-
-	    // If dropped into the bottom list, set its value to "No pref"
 
 	    // In the controller, update _all_ items (in both lists). We'll
 	    // need to ensure that the right ones end up in the right @lists,
 	    // and inspect the params a bit...
 	}
     });
+
+    //$("#ranked, #unranked").something({
+	// Change rank via the dropdown select
+    //});
 });

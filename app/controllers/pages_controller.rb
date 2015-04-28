d7d5d9362fd0b2c78687a2a6a59028c6ed1d6d00
Stefan Lance <stefan@lances.net>
2015-04-28 15:50:34 -0500

    Make homepage redirect to active quarter's courses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, except: :home
 
   def home
-    render 'courses/index'
+    y = Quarter.active_quarter.year
+    s = Quarter.active_quarter.season
+    redirect_to controller: :courses, action: :index, year: y, season: s
   end
 
 end
